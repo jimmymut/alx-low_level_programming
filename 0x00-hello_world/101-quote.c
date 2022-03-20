1b518d25d8d583a3c1ae84b95d28bbeeab22fe50
root <mutabazijimmy9@gmail.com>
2022-03-19 17:38:24 -0700

    own revision

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main()
+/**
+ *main - print exactly the way it is.
+ *
+ *Return: return 1, always (success)
+ */
+int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
