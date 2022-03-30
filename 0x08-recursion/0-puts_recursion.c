419e55cea818edcf4e59629e7009fdfa9433f92b
root <mutabazijimmy9@gmail.com>
2022-03-30 13:23:41 -0700

    sweating

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_puts_recursion - prints the string.
+ *@s: string
+ *Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+	return;
+}
