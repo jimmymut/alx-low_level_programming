8e256551ee9f3bceac90e90e3420b2de6867504b
root <mutabazijimmy9@gmail.com>
2022-03-30 16:29:05 -0700

    work

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,1 +1,14 @@
 #include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ *_print_rev_recursion - returns the value the string.
+ *@s: The string
+ *Return: The value of string.
+ */
+int _strlen_recursion(char *s)
+{
+	int i = strlen(s);
+
+	return (i);
+}
