0ddb857e1f07c70c23879a031123cac999f33582
root <mutabazijimmy9@gmail.com>
2022-03-30 15:21:13 -0700

    work

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,17 @@
 #include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ *_print_rev_recursion - reverses the string.
+ *@s: The string
+ *Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+int i;
+	for (i = strlen(s); i > 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
+}
