cc6528564132e168ed37b65998001eeef26d9d62
root <mutabazijimmy9@gmail.com>
2022-04-04 14:46:23 -0700

    Malloc and free

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void _putchar(char c);
+char **strtow(char *str);
+char *argstostr(int ac, char **av);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
+
+#endif
