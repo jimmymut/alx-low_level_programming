76687896eee9df17da50f9292440b53dcf4d1676
root <mutabazijimmy9@gmail.com>
2022-04-06 19:58:00 -0700

    tough

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -56,44 +56,44 @@ void errors(void)
  */
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	char *num1, *num2;
+	int len1, len2, len, i, carry, digit1, digit2, *mul, a = 0;
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+	num1 = argv[1], num2 = argv[2];
+	if (argc != 3 || !is_digit(num1) || !is_digit(num2))
 		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
 	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
+	mul = malloc(sizeof(int) * len);
+	if (!mul)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
-		result[i] = 0;
+		mul[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 = s1[len1] - '0';
+		digit1 = num1[len1] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		for (len2 = _strlen(num2) - 1; len2 >= 0; len2--)
 		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
+			digit2 = num2[len2] - '0';
+			carry += mul[len1 + len2 + 1] + (digit1 * digit2);
+			mul[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
+			mul[len1 + len2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
 	{
-		if (result[i])
+		if (mul[i])
 			a = 1;
 		if (a)
-			_putchar(result[i] + '0');
+			_putchar(mul[i] + '0');
 	}
 	if (!a)
 		_putchar('0');
 	_putchar('\n');
-	free(result);
+	free(mul);
 	return (0);
 }
