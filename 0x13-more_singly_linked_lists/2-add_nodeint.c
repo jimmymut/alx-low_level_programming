f528bdddaedbccb89e6fb2baaa47423cff7f80dc
root <mutabazijimmy9@gmail.com>
2022-04-26 03:17:18 -0700

    add node at the beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,13 +13,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	if (!nodebeg)
 		return (NULL);
-	else
-	{
-		nodebeg->n = n;
-		nodebeg->next = NULL;
 
-		nodebeg->next = *head;
-		*head = nodebeg;
-	}
+	nodebeg->n = n;
+	nodebeg->next = NULL;
+
+	nodebeg->next = *head;
+	*head = nodebeg;
 	return (*head);
 }
