3535c65e8ac059949e5a3bb8f47b8d2a671dd10c
root <mutabazijimmy9@gmail.com>
2022-04-26 12:04:08 -0700

    more work is done

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,1 +1,23 @@
 #include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a linked list
+ * @head: head of a list.
+ *
+ * Return: data of the deleted head node.
+ */
+int pop_listint(listint_t **head)
+{
+	int i;
+	listint_t *temp;
+	listint_t *ptr;
+
+	if (*head == NULL)
+		return (0);
+	ptr = *head;
+	i = ptr->n;
+	temp = ptr->next;
+	free(ptr);
+	*head = temp;
+	return (i);
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,1 +1,48 @@
 #include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @idx: index of the list where the new node is added.
+ * @n: integer element.
+ *
+ * Return: The address of the new node, or NULL if it failed.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *newnode, *ptr;
+
+	ptr = *head;
+
+	if (idx != 0)
+	{
+		for (i = 0; i < idx - 1 && ptr != NULL; i++)
+		{
+			ptr = ptr->next;
+		}
+	}
+
+	if (ptr == NULL && idx != 0)
+		return (NULL);
+
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->n = n;
+
+	if (idx == 0)
+	{
+		newnode->next = *head;
+		*head = newnode;
+	}
+	else
+	{
+		newnode->next = h->next;
+		ptr->next = newnode;
+	}
+
+	return (newnode);
+}
