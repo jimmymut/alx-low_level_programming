9f649e57d9a7f291218eb78aed6cf2b7b2a2d9c7
root <mutabazijimmy9@gmail.com>
2022-04-26 07:45:22 -0700

    freeing

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,1 +1,21 @@
 #include "lists.h"
+
+/**
+ *free_listint2 - function that frees a listint_t list and
+ *sets the head to NULL.
+ *@head: a pointer that points to the first node of the list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tem, *jan;
+
+	if (head != NULL)
+	{
+		jan = *head;
+		while ((tem = jan) != NULL)
+		{
+			jan = jan->next;
+			free(tem);
+		}
+	}	*head = NULL;
+}
