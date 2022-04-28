c3ce9fb85a0662c6d62385ded7861105501c0ed6
root <mutabazijimmy9@gmail.com>
2022-04-28 14:30:40 -0700

    bin to un_int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 		len++;
 	for (c = len - 1; c >= 0; c--)
 	{
-		if (!b || (b[c] != '0' && b[c] != '1'))
+		if (!b || b[c] != '0' || b[c] != '1')
 			return (0);
 		if (b[c] == '1')
 		{
